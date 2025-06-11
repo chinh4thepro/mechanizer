3a05ec6fa759b550e05152e163bb7c464bbc9705
chinh4thepro <chinh4thepro@gmail.com>
2025-06-10 23:35:44 -0400

    im too used to java

--- Firmware/kmk/mechanizer.py
+++ Firmware/kmk/mechanizer.py
@@ -14,13 +14,13 @@ diode_orientation = DiodeOrientation.COL2ROW
 #split_side = SplitSide.RIGHT
 split = Split(split_type=SplitType.UART, data_pin=board.IO, data_pin2=board.IO)
 
-if (split_side == SplitSide.LEFT) {
+if split_side == SplitSide.LEFT:
     keyboard.col_pins = (board.IO1, board.IO15, board.IO2, board.IO16, board.IO3, board.IO17)
     keyboard.row_pins = (board.IO11, board.IO23, board.IO10, board.IO22, board.IO9)
-} else {
+else:
     keyboard.col_pins = (board.IO11, board.IO23, board.IO10, board.IO22, board.IO9, board.IO21)
     keyboard.row_pins = (board.IO1, board.IO15, board.IO2, board.IO16, board.IO3)
-}
+
 
 keyboard.keymap = [
     [
